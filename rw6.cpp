69c0276c16bc2d3fc0913d7f8be7fc342948bcf7
jses <jses1010135@gmail.com>
2024-11-29 14:24:55 +0800

    1

--- rw6.cpp
+++ rw6.cpp
@@ -6,27 +6,46 @@
 // rw6.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
 //
 
-#include <iostream>
+
 
 int main()
 {
-	int *s; // 宣告指標變數 s
+	
+	FILE* file; // 宣告檔案指標
+	errno_t err = fopen_s(&file,"input.txt", "w"); // 開啟檔案
+	if (err != 0) { // 如果開啟檔案失敗
+		std::cout << "開啟檔案失敗！" << std::endl; // 輸出錯誤訊息
+		return 0; // 程式結束
+	}
+
+	int a = 0; // 宣告整數變數 a
+	int* s; // 宣告指標變數 s
 	int n; // 宣告整數變數 n
-	std::cin >> n; // 從標準輸入讀取整數 n
+	std::cin >> n; // 從標準輸入讀取整數 n m,
 
 	s = new int[n]; // 動態分配 n 個整數的陣列
 
-	for (int i = 0; i < n; i++)
-	{
-		std::cin >> s[i]; // 從標準輸入讀取 n 個整數並存入陣列 s 中
-	}
+	for (int i = 0; i < n; i++)std::cin >> s[i]; // 從標準輸入讀取 n 個整數並存入陣列 s 中
 	
-	for (int j = 0; j < n; j++)
-	{
-		std::cout << s[j] << "\n "; // 將陣列 s 中的 n 個整數輸出到標準輸出
+	for (int i = 0; i < n-1; i++) // 進行氣泡排序
+		for (int j = 0; j < n - 1 - i; j++)
+			if (s[j] > s[j + 1]) {
+				int temp = s[j];
+				s[j] = s[j + 1];
+				s[j + 1] = temp;
+			}
+		
+	
+	
+	
+	for (int i = 0; i < n; i++)fprintf(file, "%d", s[i]); // 將陣列 s 中的 n 個整數輸出到檔案
+
+		
+	
+	delete[]s; // 釋放動態分配的記憶體*\/
+    fclose(file);
 	}
-	delete[] s; // 釋放動態分配的記憶體
-}
+
     
 
 
